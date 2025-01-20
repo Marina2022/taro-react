6d48a3fe4240f0a22cc038bfe9892617584c7d07
Marina2022 <marusiiiia@yandex.ru>
2025-01-20 19:46:10 +0300

    fix

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,15 +2,16 @@ import s from './HomePage.module.scss';
 import DayBlock from "@/components/HomePage/DayBlock/DayBlock.jsx";
 import NatalMap from "@/components/HomePage/NatalMap/NatalMap.jsx";
 import ButtonWithBeak from "@/components/ui/systemComponents/ButtonWithBeak/ButtonWithBeak.jsx";
-import messagesIcon from "@/assets/img/home/messages.png"
-import specialistIcon from "@/assets/img/home/specialistIcon.png"
-import loveIcon from "@/assets/img/home/loveIcon.png"
 import {useEffect} from "react";
 import {useLocation} from "react-router-dom";
-import log from "eslint-plugin-react/lib/util/log.js";
 import {useAppContext} from "@/context/appContext.jsx";
 import Clock from "@/components/ui/Clock/Clock.jsx";
 import WaitingBar from "@/components/HomePage/WaitingBar/WaitingBar.jsx";
+
+import messagesIcon from "@/assets/img/home/messages.png"
+import specialistIcon from "@/assets/img/home/specialistIcon.png"
+import loveIcon from "@/assets/img/home/loveIcon.png"
+import askIcon from "@/assets/img/home/askIcon.png"
 
 const HomePage = () => {
   
@@ -49,7 +50,7 @@ const HomePage = () => {
               title="Спросить у таро"
               description="Ответ на любой вопрос"
               href="/"
-              img={messagesIcon}
+              img={askIcon}
             />
 
             <ButtonWithBeak
