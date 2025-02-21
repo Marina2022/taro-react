b6d8be186d0350f8dfd6b84d0319e2295eeb8917
Marina2022 <marusiiiia@yandex.ru>
2025-02-21 11:34:00 +0300

    icons-fix

--- src/components/NatalFeaturePage/NatalFeaturePage.jsx
+++ src/components/NatalFeaturePage/NatalFeaturePage.jsx
@@ -51,7 +51,7 @@ const NatalFeaturePage = () => {
                 <TextBlock key={i} classname={s.textBlock}>
                   <div className={s.headerBlock}>
                     <div className={s.imgWrapper}>
-                      <img className={s.iconImg} src={`https://my.aspectum.app/${text.icon}`} alt="icon"/>
+                      <img className={s.iconImg} src={`https://my.aspectum.app${text.icon}`} alt="icon"/>
                     </div>
                     <Header20 classname={s.textTitle}>{text.header}</Header20>
                   </div>
